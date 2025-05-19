c10dbe3c689557d4380be2bc8d66600944858238
Personal Account <145331127+adamm-alaoui@users.noreply.github.com>
2025-05-19 23:07:21 +0200

    Update configuration.sh

--- docker/configuration.sh
+++ docker/configuration.sh
@@ -41,17 +41,4 @@ newgrp docker <<EOF
 echo "New group applied."
 EOF
 
-log "Step 4: Verifying Docker installation by running hello-world container..."
-if docker run hello-world; then
-  log "Docker hello-world ran successfully."
-else
-  log "Warning: Docker hello-world did not run successfully. You might need to reboot your system."
-fi
-
-log "Step 5: Pulling MicroSim Ceram worker Docker image..."
-docker pull adammalaoui/microsim-ceram
-
-log "Listing Docker images to verify..."
-docker images | grep adammalaoui/microsim-ceram && log "MicroSim Ceram image pulled successfully." || log "Failed to find MicroSim Ceram image."
-
-log "Script finished."
+log "Restart your computer for the changes to take action"
